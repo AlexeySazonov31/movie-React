583390496c8530579093fe4f228f761d5a70df5a
Sazonov Alexey <sazonleha010203@gmail.com>
2022-06-17 18:08:47 +0300

    Button go to top

--- src/layout/Movies.js
+++ src/layout/Movies.js
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import { useState } from "react";
+import $ from 'jquery';
 
 function Movies(props) {
   const [amountCard, setAmountCard] = useState(15);
@@ -11,50 +12,76 @@ function Movies(props) {
     let pastID;
     elem = props.movies.map((movie, item) => {
       if (item < amountCard) {
-        if( pastID !==  ( movie.imdbID ? movie.imdbID : movie.id )){
+        if (pastID !== (movie.imdbID ? movie.imdbID : movie.id)) {
           pastID = movie.imdbID ? movie.imdbID : movie.id;
-          return <Card
-          key={(movie.imdbID ? movie.imdbID : movie.id )}
-          handleReadMore={props.handleReadMore}
-          movie={movie}
-        />;
+          return (
+            <Card
+              key={movie.imdbID ? movie.imdbID : movie.id}
+              handleReadMore={props.handleReadMore}
+              movie={movie}
+            />
+          );
         }
-        
       }
     });
   } else {
-    elem = <div className="error"><span>Error:</span><br/>Nothing found</div>
+    elem = (
+      <div className="error">
+        <span>Error:</span>
+        <br />
+        Nothing found
+      </div>
+    );
+  }
+
+  let classNameTopButton;
+  if ( amountCard >= props.movies.length ) {
+    classNameTopButton = 'buttonShow go_to_top';
+  } else {
+    classNameTopButton = 'buttonShow go_to_top leftBorder';
   }
 
   return (
     <div className="movies">
-      {amountCard >= props.movies.length ? (
-        <></>
-      ) : (
-        <button
-          className="buttonShow"
-          onClick={() => {
-            setAmountCard(props.movies.length);
-          }}
-        >
-          show all
-        </button>
-      )}
-      <div className="moviesContainer">
-          {elem}
+      <div className="buttonFlex">
+        {amountCard >= props.movies.length ? (
+          <></>
+        ) : (
+          <button
+            className="buttonShow"
+            onClick={() => {
+              setAmountCard(props.movies.length);
+            }}
+          >
+            show all
+          </button>
+        )}
       </div>
-      {amountCard >= props.movies.length ? (
-        <></>
-      ) : (
-        <button
-          className="buttonShow"
-          onClick={() => {
-            setAmountCard(amountCard + 15);
-          }}
-        >
-          show more
-        </button>
-      )}
+
+      <div className="moviesContainer">{elem}</div>
+      <div className="buttonFlex">
+        {amountCard >= props.movies.length ? (
+          <></>
+        ) : (
+          <button
+            className="buttonShow"
+            onClick={() => {
+              setAmountCard(amountCard + 15);
+            }}
+          >
+            show more
+          </button>
+        )}
+        <button 
+          className={classNameTopButton}
+          onClick={ () => {
+            $(document).on("click", ".go_to_top", function(e) {
+              e.preventDefault();
+              $('body, html').animate({scrollTop: 0}, 800);
+          });
+          } }
+          >go to top</button>
+      </div>
     </div>
   );
 }
